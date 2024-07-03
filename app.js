ed8f0e8f33c782fa15d108525fd545370a3c4395
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-03 08:16:07 +0300

    Updated navigation link styles

--- app.js
+++ app.js
@@ -9,24 +9,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
     });
 
     // Smooth scroll and highlight clicked menu
-    document.querySelectorAll('.nav-menu a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(anchor => {
+        anchor.addEventListener('click', function() {
+            // Highlight clicked menu
+            navLinks.forEach(link => link.classList.remove('active'));
+            anchor.classList.add('active');
 
-            const targetId = this.getAttribute('href');
+            const targetId = anchor.getAttribute('href');
             const targetSection = document.querySelector(targetId);
 
             window.scrollTo({
                 top: targetSection.offsetTop,
                 behavior: 'smooth'
-            });
-
-            // Highlight clicked menu
-            document.querySelectorAll('.nav-menu a').forEach(link => {
-                link.classList.remove('active');
-            });
-
-            this.classList.add('active');
+            });            
 
             // Close the menu if it's open
             hamburger.classList.remove("active");
